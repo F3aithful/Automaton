e3521b377ad3a255f7b04348562c4aa1538167a3
F3aithful <ebnymicrosoft@gmail.com>
2026-01-25 14:49:36 +0300

    lesson6_comm2

--- lesson_06/lesson_06.task1.py
+++ lesson_06/lesson_06.task1.py
@@ -2,16 +2,20 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
 driver.get("http://uitestingplayground.com/ajax")
 driver.maximize_window()
 
-button = driver.find_element(By.CSS_SELECTOR, 'button[class="btn btn-primary"]')
+button = driver.find_element(By.CSS_SELECTOR, 'button#ajaxButton')
 button.click()
 
-driver.implicitly_wait(16)
+waiter = WebDriverWait(driver, 15).until(
+        EC.visibility_of_element_located((By.CLASS_NAME, "bg-success"))
+    )
 text = driver.find_element(By.CSS_SELECTOR, 'p[class="bg-success"]').text
 
 print(text)
